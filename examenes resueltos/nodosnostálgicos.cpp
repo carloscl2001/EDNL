d75033a0d29a020f6ac17c0d6897947e35081b71
carloscl2001 <carloscorteslora@gmail.com>
2022-03-16 14:25:05 +0100

    l

---
+++ examenes resueltos/nodosnostálgicos.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream> 
+#include "arbol.h"
+#include "abin_E-S.h"
+
+template <typename T>
+int nNodosNostal(Abin<T>& A)
+{
+    return nNodosNostal_Rec(Abin<T>& A, typename Abin<T>::NODO_NULO)
+}
+
+template <typename T>
+int nNodosNostal_Rec(Abin<T>& A, typename Abin<T>::NODO_NULO)
+{
+    if (n == Abin<T>::NODO_NULO)
+    {
+        return 0;
+    }
+    else
+    {
+        if()
+    }
+}
+
+
+using namespace std;
+typedef char tElto;
+const tElto fin = '#'; // Fin de lectura.
+int main(){
+    
+    Abin<tElto> A, B;
+    cout << "*** Lectura del arbol binario A ***\n";
+    rellenarAbin(A, fin); // Desde std::cin
+    ofstream fs("abin.dat"); // Abrir fichero de salida.
+    imprimirAbin(fs, A, fin); // En fichero.
+    fs.close();
+    cout << "\n*** Arbol A guardado en fichero abin.dat ***\n";
+    cout << "\n*** Lectura de arbol binario B de abin.dat ***\n";
+    ifstream fe("abin.dat"); // Abrir fichero de entrada.
+    rellenarAbin(fe, B); // Desde fichero.
+    fe.close();
+    cout << "\n*** Mostrar arbol binario B ***\n";
+    imprimirAbin(B); // En std::cout
+    cout<<"-------------------- NODO VERDES CON 3 NIETOS --------------------"<<endl;
+    cout<<"\tNumero de nodos: "<<nNodosNostal(A)<<endl;
+}
