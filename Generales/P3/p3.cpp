2a09ac37134afbd97583de4f74431e12b976ba18
carloscl2001 <carloscorteslora@gmail.com>
2022-04-23 15:12:14 +0200

    hola

--- Generales/P3/p3.cpp
+++ Generales/P3/p3.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 typedef char tElto;
 const tElto fin = '#'; // fin de lectura
+
 int main (){
- Agen<tElto> A;
- ifstream fe("agen.dat"); // Abrir fichero de entrada.
- rellenarAgen(fe, A); // Desde fichero.
+    Agen<tElto> A;
+    ifstream fe("agen.dat"); // Abrir fichero de entrada.
+    rellenarAgen(fe, A); // Desde fichero.
 
- fe.close();
+    fe.close();
 
- //int n = gradoAgen(A);
-//std::cout << n << std::endl;
- //std::cout << profundidadNodoAgen(A, A.hijoIzqdo(A.hijoIzqdo(A.raiz())));
+    //int n = gradoAgen(A);
+    //std::cout << n << std::endl;
+    //std::cout << profundidadNodoAgen(A, A.hijoIzqdo(A.hijoIzqdo(A.raiz())));
 
     //podaAgen('g',A);
-   
- imprimirAgen(A); // En std::cout
+    
+    imprimirAgen(A); // En std::cout
 }
