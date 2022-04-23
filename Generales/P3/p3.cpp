4b4b570e9b2d3375ac2bc141a4813b6dc179c883
carloscl2001 <carloscorteslora@gmail.com>
2022-04-23 14:15:01 +0200

    gf

--- Generales/P3/p3.cpp
+++ Generales/P3/p3.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 typedef char tElto;
 const tElto fin = '#'; // fin de lectura
-int main ()
-{
- Agen<tElto> A(16), B(16);
- cout << "*** Lectura del árbol A ***\n";
- rellenarAgen(A, fin); // Desde std::cin
- //ofstream fs(“agen.dat”); // Abrir fichero de salida.
- //imprimirAgen(fs, A, fin); // En fichero.
- //fs.close();
- cout << "\n*** Árbol A guardado en fichero agen.dat ***\n";
- cout << "\n*** Lectura de árbol B de agen.dat ***\n";
- //ifstream fe(“agen.dat”); // Abrir fichero de entrada.
- //rellenarAgen(fe, B); // Desde fichero.
- //fe.close();
- cout << "\n*** Mostrar árbol B ***\n";
- imprimirAgen(B); // En std::cout
-} 
+int main (){
+ Agen<tElto> A;
+ ifstream fe("agen.dat"); // Abrir fichero de entrada.
+ rellenarAgen(fe, A); // Desde fichero.
+
+ fe.close();
+
+ //int n = gradoAgen(A);
+//std::cout << n << std::endl;
+ //std::cout << profundidadNodoAgen(A, A.hijoIzqdo(A.hijoIzqdo(A.raiz())));
+
+    //podaAgen('g',A);
+   
+ imprimirAgen(A); // En std::cout
+}
