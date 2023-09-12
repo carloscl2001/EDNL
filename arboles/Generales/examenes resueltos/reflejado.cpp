bbf4916b19b9066cd7e3d4c1bda933ca164e2ee6
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-09-12 18:06:32 +0200

    Create reflejado.cpp

---
+++ arboles/Generales/examenes resueltos/reflejado.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream> 
+#include "arbol.h"
+#include "abin_E-S.h"
+
+//Dado un árbol binario de un tipo genérico T calcular el número de nodos que siendo hermanos entre
+//sí tienen misma raíz y sus subárboles son reflejados (respecto a sus elementos)
+
+
+int nNodosReflejados(const Abin<T>& A){
+
+}
