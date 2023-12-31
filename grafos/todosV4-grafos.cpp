6e72ea2fe6daf426d1c24e726d1c3a8e2d164e6c
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-12-31 16:19:43 +0100

    Update todosV4-grafos.cpp

--- grafos/todosV4-grafos.cpp
+++ grafos/todosV4-grafos.cpp
@@ -1228,3 +1228,57 @@ tCoste Grecoland(const vector<ciudad> Fobos, const vector<ciudad> Deimos, const 
     std::cout << costes[destino];
 
 }
+
+
+
+/*Un repartidor de una empresa de distribución de bebidas tiene que visitar a todos sus
+clientes cada día. Pero, al comenzar su jornada de trabajo, no conoce qué cantidad de bebidas
+tiene que servir a cada cliente, por lo que no puede planificar una ruta óptima para visitarlos a
+todos. Por tanto, nuestro repartidor decide llevar a cabo la siguiente estrategia:
+ El camión parte del almacén con la máxima carga permitida rumbo a su cliente más
+próximo.
+ El repartidor descarga las cajas de bebidas que le pide el cliente. Si no tiene suficientes
+cajas en el camión, le entrega todas las que tiene. Este cliente terminará de ser servido en
+algún otro momento a lo largo del día, cuando la estrategia de reparto vuelva a llevar al
+repartidor hasta él.
+ Después de servir a un cliente:
+o Si quedan bebidas en el camión, el repartidor consulta su sistema de navegación
+basado en GPS para conocer la ruta que le lleva hasta su cliente más próximo
+pendiente de ser servido.
+o Si no quedan bebidas en el camión, vuelve al almacén por el camino más corto y
+otra vez carga el camión completamente.
+ Después de cargar el camión, el repartidor consulta su sistema de navegación y se va por
+el camino más corto a visitar al cliente pendiente de ser servido más próximo.
+Implementa un subprograma que calcule y devuelva la distancia total recorrida en un
+día por nuestro repartidor, a partir de lo siguiente:
+1. Grafo representado mediante matriz de costes con las distancias de los caminos directos
+entre los clientes y entre ellos y la central.
+2. Capacidad máxima del camión (cantidad de cajas de bebidas).
+3. Asumiremos que existe una función int Pedido() que devuelve el número de cajas que
+quedan por servir al cliente en el que se encuentra el repartidor.
+*/
+
+
+/*
+La capital de Zuelandia está alcanzando niveles de toxicidad muy elevados, por ello se ha
+decretado el cierre a la ciudad como paso de tránsito hacia otras ciudades. (Para ir de una
+ciudad a otra no se podrá pasar por C.Zuelandia, pero si se podrá ir si es residente de la misma
+empleándola como ciudad destino u origen).
+Implemente un subprograma que dada la capital y un grafo ponderado con los km de las
+carreteras existentes entre las ciudades del país, nos devuelva los caminos resultantes de
+nuestra nueva política “Sin pasar por la capital, por favor”.
+*/
+
+
+
+/*La capital de Zuelandia está ultimamente mas toxica que tu novia. Ante esta situacion
+el gobierno esta pensando en cerrar los accesos a la capital (excepto residentes).
+Esto solo se hará si los nuevos costes de ir de una ciudad a otra no superan en mas de unos kilometros dados, 
+a los costes antes de implementar la medida. Para ello nos dan los siguientes datos:
+La capital de Zuelandia
+El grafo con los kilometros de cada carretera del país
+Los viajes diarios que se estima que se realizan entre pares de ciudades
+El numero máximo del total de kilometros adicionales que se acepta realizar para implementar la medida
+
+Implementa un subprograma que calcule y devuelva el numero de km adicionales que provocaria la implantacion 
+de la medida anticontaminacion, y si debe implementarse dicha media o no */
