bfe3d0e1671b96edc93b2733576c43bc652b630f
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2024-01-14 13:55:06 +0100

    Update todosV4-grafos.cpp

--- grafos/todosV4-grafos.cpp
+++ grafos/todosV4-grafos.cpp
@@ -1340,6 +1340,61 @@ tCoste Grecoland(const vector<ciudad> Fobos, const vector<ciudad> Deimos, const 
     return vDjikstra[destino];
 }
 
+template <typename tCoste>
+tCoste Grecoland_version_mejorada(const vector<ciudad> Fobos, const vector<ciudad> Deimos, const vector<vertice> costerasFobos, 
+                const vector<vertice> costerasDeimos, vertice origen, vertice destino){
+    typedef typename GrafoP<tCoste>::vertice vertice;
+    static const double INF;
+
+    size_t nFobos = Fobos.size();
+    size_t nDeimos = Deimos.size();
+
+    double minDistancia = INF;
+    punete p;//puente que une las islas mas corto
+
+    for(vertice i = 0 ; i < costerasFobos.size(); i ++){
+        for(vertice j = 0 ; j < costerasDeimos.size(); j ++){
+            if(distancia(Fobos[costeraFobos[i]], Deimos[costeraDeimos[j]]) < minDistancia){
+                minDistancia = distancia(Fobos[costeraFobos[i]], Deimos[costeraDeimos[j]]);
+                p.a = i;
+                p.b = j;
+                p.coste = minDistancia;
+            }
+        }
+    }
+    size_t nSuper = nFobos + nDeimos;
+    Grafo<tCoste> sGrafo(nSuper);
+    Grafo<tCoste> gFinal(nSuper);
+
+
+
+    //Rellenamos el super grafo, coste irectos
+    for(size_t i = 0; i <  nFobos ; i++){
+        for(size_t j = 0 ; j < nFobos ; j++){
+            sGrafo[i][j] = distancia(Fobos[i],Fobos[j]);
+        }
+    }
+
+    for(size_t i = 0; i <  nDeimos ; i++){
+        for(size_t j = 0 ; j < nDeimos; j++){
+            sGrafo[i][j] = distancia(Deimos[i],Deimos[j]);
+        }
+    }
+
+    //señalamos el puente elegido en el supergrafo 
+    sGrafo[puente.a][puente.b+nFobos] = sGrafo[puente.b+nFobos][puente.a] = p.coste;
+
+    gFinal = Kruskal(sGrafo);
+
+    vector<vertice> V;
+    vector<tCoste> vDjikstra;
+
+    vDjikstra = Dijkstra(gFinal, origen, V);
+
+    return vDjikstra[destino];
+}
+
+
 
 /*Un repartidor de una empresa de distribución de bebidas tiene que visitar a todos sus
 clientes cada día. Pero, al comenzar su jornada de trabajo, no conoce qué cantidad de bebidas
@@ -1498,6 +1553,38 @@ matriz<tCoste> latoxica(const GrafoP<tCoste>& G, typename GrafoP<tCoste>::vertic
 }
 
 
+/*7. EN VEZ DE 2 ISLAS SON 3 ISLAS
+El archipiélago de Grecoland (Zuelandia) está formado únicamente por dos islas,
+Fobos y Deimos, que tienen N1 y N2 ciudades, respectivamente, de las cuales C1 y C2
+ciudades son costeras (obviamente C1 ≤ N1 y C2 ≤ N2 ). Se dispone de las coordenadas
+cartesianas (x, y) de todas y cada una de las ciudades del archipiélago. El huracán
+Isadore acaba de devastar el archipiélago, con lo que todas las carreteras y puentes
+construidos en su día han desaparecido. En esta terrible situación se pide ayuda a la
+ONU, que acepta reconstruir el archipiélago (es decir volver a comunicar todas las
+ciudades del archipiélago) siempre que se haga al mínimo coste.
+ De cara a poder comparar costes de posibles reconstrucciones se asume lo
+siguiente:
+1. El coste de construir cualquier carretera o cualquier puente es proporcional a su
+longitud (distancia euclídea entre las poblaciones de inicio y fin de la carretera o
+del puente).
+2. Cualquier puente que se construya siempre será más caro que cualquier carretera
+que se construya.
+ De cara a poder calcular los costes de VIAJAR entre cualquier ciudad del
+archipiélago se considerará lo siguiente:
+1. El coste directo de viajar, es decir de utilización de una carretera o de un puente,
+coincidirá con su longitud (distancia euclídea entre las poblaciones origen y
+destino de la carretera o del puente).
+ En estas condiciones, implementa un subprograma que calcule el coste mínimo
+de viajar entre dos ciudades de Grecoland, origen y destino, después de haberse
+reconstruido el archipiélago, dados los siguientes datos:
+1. Lista de ciudades de Fobos representadas mediante sus coordenadas cartesianas.
+2. Lista de ciudades de Deimos representadas mediante sus coordenadas
+cartesianas.
+3. Lista de ciudades costeras de Fobos.
+4. Lista de ciudades costeras de Deimos.
+5. Ciudad origen del viaje.
+6. Ciudad destino del viaje.
+*/
 //AJEDREZ
 
 //LABERINTO 2D
